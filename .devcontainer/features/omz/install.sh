a1e640994bbccee536b4017947634f03ce91be1a
John Martin <john.itvn@gmail.com>
2025-12-04 09:33:00 +0000

    chore: initialize repository

---
+++ .devcontainer/features/omz/install.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+set -e
+
+PRE_INSTALLED_PLUGINS=${PREINSTALLEDPLUGINS:-'git,git-auto-fetch'}
+CUSTOM_PLUGINS=${CUSTOMPLUGINS:-''}
+ACTIVE_PLUGINS=()
+DELETE_INACTIVE=${DELETEINACTIVE:-false}
+DEBUG=${DEBUG:-false}
+
+debug() {
+  if [[ ${DEBUG} == true ]]; then
+    echo "$1" >> /tmp/omz-plugin.log
+  fi
+}
+
+if [[ -n ${_REMOTE_USER_HOME} ]]; then
+  USER_HOME="${_REMOTE_USER_HOME}"
+elif [[ ${_REMOTE_USER} == "root" ]]; then
+  USER_HOME="/root"
+# Check if user already has a home directory other than /home/${USERNAME}
+elif [[ "/home/${_REMOTE_USER}" != $(getent passwd "${_REMOTE_USER}" | cut -d: -f6) ]]; then
+  USER_HOME=$(getent passwd "${_REMOTE_USER}" | cut -d: -f6)
+else
+  USER_HOME="/home/${_REMOTE_USER}"
+fi
+
+debug "==============================="
+debug "PRE_INSTALLED_PLUGINS: ${PRE_INSTALLED_PLUGINS}"
+debug "CUSTOM_PLUGINS: ${CUSTOM_PLUGINS}"
+debug "REMOTE_USER: ${_REMOTE_USER}"
+debug "USER_HOME: ${USER_HOME}"
+debug "==============================="
+
+IFS=',' read -r -a plugins_array <<< "${CUSTOM_PLUGINS}"
+for plugin in "${plugins_array[@]}"; do
+  name=$(echo "${plugin}" | cut -d':' -f1)
+  url=$(echo "${plugin}" | cut -d':' -f2-)
+  debug " - Installing plugin: ${name} from ${url}"
+  plugin_dir="${USER_HOME}/.oh-my-zsh/custom/plugins/${name}"
+  mkdir -p "${plugin_dir}"
+  debug "    - Plugin directory: ${plugin_dir}"
+
+  if [[ ${url} == *.git ]]; then
+    debug "    - Cloning plugin from git"
+    git clone --depth=1 "${url}" "${plugin_dir}"
+  elif [[ ${url} == *.zip ]]; then
+    debug "    - Downloading plugin from zip"
+    curl -L "${url}" -o /tmp/plugin.zip
+    zip_content=$(unzip -Z1 /tmp/plugin.zip)
+    base_dir=$(echo "${zip_content}" | head -n 1)
+    unzip /tmp/plugin.zip -d /tmp/plugin
+    mv /tmp/plugin/"${base_dir}"/* "${plugin_dir}/"
+    rm /tmp/plugin.zip
+    rm -r /tmp/plugin
+  else
+    echo "Unsupported URL format: ${url}"
+    exit 1
+  fi
+
+  ACTIVE_PLUGINS+=("${name}")
+done
+
+IFS=',' read -r -a plugins_array <<< "${PRE_INSTALLED_PLUGINS}"
+for plugin in "${plugins_array[@]}"; do
+  ACTIVE_PLUGINS+=("${plugin}")
+done
+
+ACTIVE_PLUGINS_STRING=$(printf " %s" "${ACTIVE_PLUGINS[@]}")
+
+sed -i "s/plugins=(.*)/plugins=(${ACTIVE_PLUGINS_STRING:1})/g" "${USER_HOME}/.zshrc"
+
+if [[ ${DELETE_INACTIVE} == false ]]; then
+  exit 0
+fi
+
+PLUGINS_DIRS=("${USER_HOME}/.oh-my-zsh/custom/plugins" "${USER_HOME}/.oh-my-zsh/plugins")
+for PLUGIN_DIR in "${PLUGINS_DIRS[@]}"; do
+  debug "Checking for inactive plugins in ${PLUGIN_DIR}"
+  for dir in "${PLUGIN_DIR}"/*; do
+    debug " - Checking plugin: ${dir}"
+    dir_name=$(basename "${dir}")
+    debug "    - Plugin name: ${dir_name}"
+    should_delete=true
+    for active_plugin in "${ACTIVE_PLUGINS[@]}"; do
+      if [[ ${active_plugin} == "${dir_name}" ]]; then
+        should_delete=false
+        break
+      fi
+    done
+    debug "    - Should delete: ${should_delete}"
+    if [[ ${should_delete} == true ]]; then
+      debug "    - Deleting plugin: ${dir}"
+      rm -rf "${dir}"
+    fi
+  done
+done
+
+## Integrate iTerm2 shell integration for Zsh and fix ai agent terminal issues
+CODE_TO_APPEND='
+  PROMPT_EOL_MARK=""
+  test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+  precmd() { print -Pn "\e]133;D;%?\a" }
+  preexec() { print -Pn "\e]133;C;\a" }
+'
+curl -sSL https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh || true
+echo "$CODE_TO_APPEND" >> "$USER_HOME/.zshrc"
---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,2 @@
+git config --global --unset-all core.editor
+git config --global core.editor "code --wait"
